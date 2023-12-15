8ebfa796a7b7b5a65a3cde6493e5f48d8fb379d8
Otabek1990 <fozilovotabek424@gmail.com>
2023-12-15 19:44:32 +0500

    fix-about-page-fixed

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -5,6 +5,9 @@ function About() {
     const { data, isLoading, isSuccess } = useProdQuery()
     return (
         <div className="about">
+            <h1>
+                Lorem ipsum dolor sit.
+            </h1>
             {isLoading && <h1>Loading...</h1>}
             {isSuccess && data.map(item => <Card item={item} key={item.id} />)}
             
